dde9f1908ff49636ad86237b4eaefb2d89a7316f
TTraveller7 <yinghuo777@gmail.com>
2022-06-14 12:25:12 +0800

    Initial commit.

---
+++ src/test/java/bht/salvinto/stickynotes/StickyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package bht.salvinto.stickynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
