8c4b5b8a36985633c800f19c2108a3cd8cd79b85
TTraveller7 <yinghuo777@gmail.com>
2022-06-27 17:24:01 +0800

    Refactor IndexPageController.java and index.html to display Notes. Closes #21.

--- src/main/java/bht/salvinto/stickynotes/controllers/IndexPageController.java
+++ src/main/java/bht/salvinto/stickynotes/controllers/IndexPageController.java
@@ -1,13 +1,34 @@
 package bht.salvinto.stickynotes.controllers;
 
+import bht.salvinto.stickynotes.domain.PlainNote;
+import bht.salvinto.stickynotes.domain.TodoNote;
+import bht.salvinto.stickynotes.services.PlainNoteService;
+import bht.salvinto.stickynotes.services.TodoNoteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexPageController {
 
+    private final PlainNoteService plainNoteService;
+    private final TodoNoteService todoNoteService;
+
+    public IndexPageController(PlainNoteService plainNoteService, TodoNoteService todoNoteService) {
+        this.plainNoteService = plainNoteService;
+        this.todoNoteService = todoNoteService;
+    }
+
+    @GetMapping
     @RequestMapping(value = {"", "/index", "/index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        Iterable<TodoNote> todoNotes = todoNoteService.findAll();
+        model.addAttribute("todoNotes", todoNotes);
+
+        Iterable<PlainNote> plainNotes = plainNoteService.findAll();
+        model.addAttribute("plainNotes", plainNotes);
+
         return "index";
     }
 }
