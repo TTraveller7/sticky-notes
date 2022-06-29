42cb8fc063db71c50c2024727684ce2c6e1fd863
TTraveller7 <yinghuo777@gmail.com>
2022-06-29 23:04:27 +0800

    Add saveCommand method to save TodoNoteCommand object

--- src/main/java/bht/salvinto/stickynotes/services/TodoNoteService.java
+++ src/main/java/bht/salvinto/stickynotes/services/TodoNoteService.java
@@ -1,7 +1,10 @@
 package bht.salvinto.stickynotes.services;
 
+import bht.salvinto.stickynotes.commands.TodoNoteCommand;
 import bht.salvinto.stickynotes.domain.TodoNote;
 
 public interface TodoNoteService {
     Iterable<TodoNote> findAll();
+
+    TodoNoteCommand saveCommand(TodoNoteCommand todoNoteCommand);
 }
--- src/main/java/bht/salvinto/stickynotes/services/impl/TodoNoteServiceImpl.java
+++ src/main/java/bht/salvinto/stickynotes/services/impl/TodoNoteServiceImpl.java
@@ -1,5 +1,8 @@
 package bht.salvinto.stickynotes.services.impl;
 
+import bht.salvinto.stickynotes.commands.TodoNoteCommand;
+import bht.salvinto.stickynotes.converters.TodoNoteCommandToTodoNote;
+import bht.salvinto.stickynotes.converters.TodoNoteToTodoNoteCommand;
 import bht.salvinto.stickynotes.domain.TodoNote;
 import bht.salvinto.stickynotes.repositories.TodoNoteRepository;
 import bht.salvinto.stickynotes.services.TodoNoteService;
@@ -10,12 +13,27 @@ public class TodoNoteServiceImpl implements TodoNoteService {
 
     private final TodoNoteRepository todoNoteRepository;
 
-    public TodoNoteServiceImpl(TodoNoteRepository todoNoteRepository) {
+    private final TodoNoteCommandToTodoNote todoNoteCommandToTodoNote;
+    private final TodoNoteToTodoNoteCommand todoNoteToTodoNoteCommand;
+
+    public TodoNoteServiceImpl(TodoNoteRepository todoNoteRepository,
+            TodoNoteCommandToTodoNote todoNoteCommandToTodoNote,
+            TodoNoteToTodoNoteCommand todoNoteToTodoNoteCommand) {
         this.todoNoteRepository = todoNoteRepository;
+        this.todoNoteCommandToTodoNote = todoNoteCommandToTodoNote;
+        this.todoNoteToTodoNoteCommand = todoNoteToTodoNoteCommand;
     }
 
     @Override
     public Iterable<TodoNote> findAll() {
         return todoNoteRepository.findAll();
+    }
+
+    @Override
+    public TodoNoteCommand saveCommand(TodoNoteCommand todoNoteCommand) {
+        TodoNote todoNote = todoNoteCommandToTodoNote.convert(todoNoteCommand);
+        TodoNote savedTodoNote = todoNoteRepository.save(todoNote);
+
+        return todoNoteToTodoNoteCommand.convert(savedTodoNote);
     }
 }
