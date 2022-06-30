5fc1edf8b9e92cd549708c749f006763f5a3d069
TTraveller7 <yinghuo777@gmail.com>
2022-06-30 19:55:21 +0800

    Change dashboard layout. Add Category#capitalizedName()

--- src/main/java/bht/salvinto/stickynotes/domain/Category.java
+++ src/main/java/bht/salvinto/stickynotes/domain/Category.java
@@ -1,5 +1,11 @@
 package bht.salvinto.stickynotes.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum Category {
-    WORK, SOCIALIZE, EXERCISE, MISC
+    WORK, SOCIALIZE, EXERCISE, MISC;
+
+    public String capitalizedName() {
+        return StringUtils.capitalize(this.name().toLowerCase());
+    }
 }
