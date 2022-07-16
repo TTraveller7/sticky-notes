1967c5d3c01a4bc84d1e10419d34da500023ee30
TTraveller7 <yinghuo777@gmail.com>
2022-07-16 23:37:42 +0800

    Remove files and classes related to template rendering. Closes #22.

--- src/main/java/bht/salvinto/stickynotes/ui/states/PlainNoteState.java
+++
@@ -1,5 +0,0 @@
-package bht.salvinto.stickynotes.ui.states;
-
-public enum PlainNoteState {
-    DEFAULT, DELETE, CREATE, EDIT_SELECT, EDIT_FILL;
-}
--- src/main/java/bht/salvinto/stickynotes/ui/states/TodoNoteState.java
+++
@@ -1,5 +0,0 @@
-package bht.salvinto.stickynotes.ui.states;
-
-public enum TodoNoteState {
-    DEFAULT, DELETE, CREATE, EDIT_SELECT, EDIT_FILL
-}
