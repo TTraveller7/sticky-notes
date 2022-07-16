f47f13319a2aab992173f5e4b0b35d18a6601d85
TTraveller7 <yinghuo777@gmail.com>
2022-07-16 23:40:42 +0800

    Refactor import.sql.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
 -- insert plain_note
-insert into Note(dtype, title, description, creation_timestamp)
-values('plain_note', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2022-06-18');
-insert into Note(dtype, title, description, creation_timestamp)
-values('plain_note', 'Neque porro', 'Lquisquam est qui dolorem ipsum quia dolor sit amet, consectetur', '2022-06-18');
+insert into PLAIN_NOTE(title, description, creation_timestamp)
+values('Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2022-06-18');
+insert into PLAIN_NOTE(title, description, creation_timestamp)
+values('Neque porro', 'Lquisquam est qui dolorem ipsum quia dolor sit amet, consectetur', '2022-06-18');
 
 -- insert todo_note
-insert into Note(dtype, title, description, creation_timestamp, category, status)
-values('todo_note', 'Spring', 'CRUD operations sec 11', '2022-06-18', 'WORK', 'TODO');
-insert into Note(dtype, title, description, creation_timestamp, category, status)
-values('todo_note', 'Wash clothes', 'Wash all the clothes in the morning '
+insert into TODO_NOTE(title, description, creation_timestamp, category, status)
+values('Spring', 'CRUD operations sec 11', '2022-06-18', 'WORK', 'TODO');
+insert into TODO_NOTE(title, description, creation_timestamp, category, status)
+values('Wash clothes', 'Wash all the clothes in the morning '
     || 'so that they could be dry before dinner', '2022-06-17', 'MISC', 'DONE');
